c37b82890d6b96f5f92e8b598c712ed657492d58
Teri Chudějová <tereza.chudejova@gmail.com>
2024-08-06 17:14:19 +0200

    add first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import "./index.css"
+import HamburgerMenu from "./HamburgerMenu"
 
 function App() {
 
@@ -11,33 +12,61 @@ function App() {
       <header className='navbar outerBox'>
         <div className='innerBox'>
             <h1>Kalorická Kalkulačka</h1>
-            <p>Menu</p>
+            <HamburgerMenu/>
         </div>
       </header>
 
       {/* FIRST SECTION */}
-      <div className='firstSection outerBox'>
+      <div className='firstSection outerBox' id='firstSection'>
         <div className='innerBox'>
-
+          <div className='uvod'>
+            <h2>Naučte se pracovat s kaloriemi už jednou provždy!</h2>
+            <p>
+              Prvním krokem k úspěchu, ať už je váš cíl zhubnout nebo nabrat, je porozumět <span>kaloriím</span>,
+              kalorickému <span>deficitu</span>, <span>nadbytku</span>, <span>bazálnímu metabolismu</span> a dalším důležitým pojmům.
+            </p>
+            <p>
+              Každá potravina má určitý počet kalorií. To, kolik za celý den sníme a vypijeme kalorií, se nazývá náš kalorický příjem.
+              Máme však i svůj kalorický výdej. Ten se skládá z tzv. bazálního metabolismu a dalších aktivit.
+              Mezi další aktivity řadíme veškerý pohyb, ať už je to cesta do práce, uklízení nebo třeba trénink ve fitku. Čím více
+              se samozřejmě hýbeme, tím vyšší je náš kalorický výdej.
+            </p>
+            <p>
+              Hubnutí nebo nabírání pak funguje jako prostá rovnice. Pokud náš kalorický příjem převýší náš kalorický výdej, jedná se o kalorický nadbytek
+              a tělo začne přibírat. Pokud je náš kalorický příjem naopak nižší než náš kalorický výdej, jedná se o kalorický deficit a v této fázi tělo hubne.
+            </p>
+            <p>
+              Je však nanejvýš důležité si uvědomit, že <b>náš minimální kalorický příjem musí odpovídat bazálnímu metabolismu</b>. Pokud bychom přijímali méně kalorií,
+              než je náš bazální metabolismus, naše tělo by nemělo dostatek energie na základní obsluhování našich orgánů a to je velmi nebezpečné.
+            </p>
+          </div>
+          <img id='foodPlate' src='images/foodPlate.png'/>
         </div>
       </div>
 
       {/* SECOND SECTION */}
-      <div className='secondSection outerBox'>
+      <div className='secondSection outerBox' id='secondSection'>
         <div className='innerBox'>
 
         </div>
       </div>
 
       {/* THIRD SECTION */}
-      <div className='thirdSection outerBox'>
+      <div className='thirdSection outerBox' id='thirdSection'>
+        <div className='innerBox'>
+
+        </div>
+      </div>
+
+      {/* FOURTH SECTION */}
+      <div className='fourthSection outerBox' id='fourthSection'>
         <div className='innerBox'>
 
         </div>
       </div>
 
       {/* FOOTER */}
-      <footer className='footer outerBox'>
+      <footer className='footer outerBox' id='footer'>
         <div className='innerBox'>
 
         </div>
---
+++ src/HamburgerMenu.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import "./HamburgerMenu.css"
+
+export default function NavigationBar() {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const handleToggle = () => {
+      setIsOpen(!isOpen);
+    };
+  
+    return (
+      <div className="hamburger-menu">
+        <div className={`menu-toggle ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <nav className={`nav-menu ${isOpen ? 'open' : ''}`}>
+          <a href="#firstSection">Jak to funguje?</a>
+          <a href="#secondSection">Spočítat kalorie</a>
+          <a href="#thirdSection">Co dál?</a>
+          <a href='#fourthSection'>Chci vědět více</a>
+          <a href='#footer'>Kontakt</a>
+        </nav>
+      </div>
+    );
+  }
