76b4068f235e7cb15179273f657aff791b8b48e2
Teri Chudějová <tereza.chudejova@gmail.com>
2024-08-06 18:07:38 +0200

    add second section - title, image and lorem ipsum p

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <div className='firstSection outerBox' id='firstSection'>
         <div className='innerBox'>
           <div className='uvod'>
-            <h2>Naučte se pracovat s kaloriemi už jednou provždy!</h2>
+            <h2>Nauč se pracovat s kaloriemi už jednou provždy!</h2>
             <p>
               Prvním krokem k úspěchu, ať už je váš cíl zhubnout nebo nabrat, je porozumět <span>kaloriím</span>,
               kalorickému <span>deficitu</span>, <span>nadbytku</span>, <span>bazálnímu metabolismu</span> a dalším důležitým pojmům.
@@ -36,7 +36,7 @@ function App() {
               a tělo začne přibírat. Pokud je náš kalorický příjem naopak nižší než náš kalorický výdej, jedná se o kalorický deficit a v této fázi tělo hubne.
             </p>
             <p>
-              Je však nanejvýš důležité si uvědomit, že <b>náš minimální kalorický příjem musí odpovídat bazálnímu metabolismu</b>. Pokud bychom přijímali méně kalorií,
+              Je však nanejvýš důležité si uvědomit, že <b>náš minimální kalorický příjem musí přesahovat bazální metabolismus</b>. Pokud bychom přijímali méně kalorií,
               než je náš bazální metabolismus, naše tělo by nemělo dostatek energie na základní obsluhování našich orgánů a to je velmi nebezpečné.
             </p>
           </div>
@@ -47,7 +47,14 @@ function App() {
       {/* SECOND SECTION */}
       <div className='secondSection outerBox' id='secondSection'>
         <div className='innerBox'>
-
+          <img id='pokeBowl' src='images/poke.png'/>
+          <div className='jakPocitat'>
+            <h2>Spočítej si bazální metabolismus</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In ducimus deleniti quisquam dolore inventore culpa autem maiores iure blanditiis earum, quidem, praesentium dolorem! Obcaecati quidem saepe similique voluptates et adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nihil, voluptatum tenetur, qui fugiat sint facilis doloribus doloremque a, tempora numquam dolores totam error voluptatem veniam impedit assumenda illum sit? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dolores aut animi fuga vitae voluptate minus sed quisquam adipisci veritatis! Magnam ea dignissimos est perspiciatis, quae enim odio officia corrupti?</p>
+            <div className='kalkulacka'>
+              <h3>Kalkulačka</h3>
+            </div>
+          </div>
         </div>
       </div>
 
