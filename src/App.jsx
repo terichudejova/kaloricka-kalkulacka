3801bddab97821e95ca4cdfaa2bb89bed5d53ea5
Teri Chudějová <tereza.chudejova@gmail.com>
2024-08-06 14:31:28 +0200

    make a layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import "./index.css"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='mainContainer'>
+
+      {/* NAVBAR */}
+      <header className='navbar outerBox'>
+        <div className='innerBox'>
+            <h1>Kalorická Kalkulačka</h1>
+            <p>Menu</p>
+        </div>
+      </header>
+
+      {/* FIRST SECTION */}
+      <div className='firstSection outerBox'>
+        <div className='innerBox'>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {/* SECOND SECTION */}
+      <div className='secondSection outerBox'>
+        <div className='innerBox'>
+
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      {/* THIRD SECTION */}
+      <div className='thirdSection outerBox'>
+        <div className='innerBox'>
+
+        </div>
+      </div>
+
+      {/* FOOTER */}
+      <footer className='footer outerBox'>
+        <div className='innerBox'>
+
+        </div>
+      </footer>
+
+    </div>
   )
 }
 
