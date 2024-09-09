6a0a175b1d8b2355d4d976c5badb617981c23f73
Teri Chudějová <tereza.chudejova@gmail.com>
2024-09-09 21:35:35 +0200

    adjust hamburger menu, so it closes when you click on a link or somewhere on the page

--- src/HamburgerMenu.jsx
+++ src/HamburgerMenu.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import "./HamburgerMenu.css"
 
 export default function NavigationBar() {
     const [isOpen, setIsOpen] = useState(false);
+    const navRef = useRef(null);
   
     const handleToggle = () => {
       setIsOpen(!isOpen);
     };
+
+    const handleCloseMenu = () => {
+      setIsOpen(false);
+    }
+
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+          if (navRef.current && !navRef.current.contains(event.target)) {
+              setIsOpen(false);
+          }
+      };
+
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, []);
   
     return (
       <div className="hamburger-menu">
@@ -16,10 +34,10 @@ export default function NavigationBar() {
           <div className="bar"></div>
           <div className="bar"></div>
         </div>
-        <nav className={`nav-menu ${isOpen ? 'open' : ''}`}>
-          <a href="#firstSection">Jak to funguje?</a>
-          <a href="#secondSection">Spočítat kalorie</a>
-          <a href="#thirdSection">Chci vědět více</a>
+        <nav ref={navRef} className={`nav-menu ${isOpen ? 'open' : ''}`}>
+          <a href="#firstSection" onClick={handleCloseMenu}>Jak to funguje?</a>
+          <a href="#secondSection" onClick={handleCloseMenu}>Spočítat kalorie</a>
+          <a href="#thirdSection" onClick={handleCloseMenu}>Chci vědět více</a>
         </nav>
       </div>
     );
